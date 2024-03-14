ed4d90e87d0728ce486e9c55eb1dbc0a06db0648
kev-techi <kevin@techi.be>
2024-03-14 13:14:03 +0100

    add asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_BASE_ASSET_PATH
+      : undefined,
 };
 
 export default nextConfig;
