d5424452f129dd0ab5ff4928f6238f2d0fc548c8
sajee_techi <sajeewa@techi.be>
2025-03-10 21:42:36 +0530

    otp login - verifiy otp - create auth session

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,11 @@
 import { NextAuthConfig } from 'next-auth';
 import CredentialProvider from 'next-auth/providers/credentials';
 import { getServiceRoleClient } from '@/services/db';
+import { getOTPOfSource, deleteOTPOfSource } from '@/services/db/otp';
+import { getAdminByEmail } from '@/services/db/admin';
 
 const authConfig = {
+
   providers: [
     CredentialProvider({
       name: 'OTP Login',
@@ -23,20 +26,54 @@ const authConfig = {
         }
 
         const client = getServiceRoleClient(chainId);
+        const { data, error } = await getOTPOfSource({ client, source: email });
+
+        if (error) {
+          throw new Error(`Failed to get login code for email ${email}`);
+        }
+
+        if (!data) {
+          throw new Error(`No login code found for email ${email}`);
+        }
+
+        if (data.expires_at < new Date()) {
+          throw new Error(`Login code expired for email ${email}`);
+        }
+
+        if (data.code !== code) {
+          throw new Error(`Invalid login code for email ${email}`);
+        }
+
+        const { data: adminData, error: adminError } = await getAdminByEmail({
+          client,
+          email
+        });
+
+        if (adminError) {
+          throw new Error(`Failed to get admin by email ${email}`);
+        }
+
+        if (!adminData) {
+          throw new Error(`Admin not found for email ${email}`);
+        }
+
+        deleteOTPOfSource({ client, source: email });
 
         user = {
-          id: '',
-          email: '',
-          name: ''
+          id: adminData.id.toString(),
+          email: adminData.email,
+          name: adminData.name
         };
 
         return user;
       }
     })
+    
   ],
   pages: {
     signIn: '/login'
   },
+  
   callbacks: {
     jwt: async ({ token, user }) => {
       if (user) {
