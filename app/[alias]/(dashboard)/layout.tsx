6ee3b5563290df004c60b6fd1839b3cf6288d832
sajee_techi <sajeewa@techi.be>
2025-03-11 23:02:42 +0530

    search transfers with a_members

--- app/[alias]/(dashboard)/layout.tsx
+++ app/[alias]/(dashboard)/layout.tsx
@@ -8,6 +8,7 @@ import { fetchCommunitiesOfChainAction } from '@/app/_actions/community-actions'
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import { getAdminByEmailAction } from '@/app/_actions/admin-actions';
+import UrlSearch from '@/components/custom/url-search';
 
 export default async function DashboardLayout({
   children,
@@ -49,9 +50,10 @@ export default async function DashboardLayout({
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
+            <UrlSearch />
           </div>
         </header>
-        <main className="flex flex-col items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 bg-muted/40 overflow-hidden">
+        <main className="flex flex-col items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4 overflow-hidden">
           {children}
         </main>
       </SidebarInset>
