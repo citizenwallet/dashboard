724f35ff88fcc03b982b5c8605f653a1a551022d
sajee_techi <sajeewa@techi.be>
2025-03-11 10:42:43 +0530

    communities shown are restricted to access list

--- app/[alias]/(dashboard)/layout.tsx
+++ app/[alias]/(dashboard)/layout.tsx
@@ -5,6 +5,9 @@ import {
 } from '@/components/ui/sidebar';
 import { AppSidebar } from './_components/app-sidebar';
 import { fetchCommunitiesOfChainAction } from '@/app/_actions/community-actions';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import { getAdminByEmailAction } from '@/app/_actions/admin-actions';
 
 export default async function DashboardLayout({
   children,
@@ -13,12 +16,35 @@ export default async function DashboardLayout({
   children: React.ReactNode;
   params: Promise<{ alias: string }>;
 }) {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect('/login');
+  }
+
+  const admin = await getAdminByEmailAction({
+    email: session.user.email ?? '',
+    chainId: 42220
+  });
+
   const { alias } = await params;
-  const { communities } = await fetchCommunitiesOfChainAction(42220);
+
+  if (!admin?.community_access_list.includes(alias)) {
+    redirect('/');
+  }
+
+  const { communities } = await fetchCommunitiesOfChainAction({
+    chainId: 42220,
+    accessList: admin.community_access_list
+  });
 
   return (
     <SidebarProvider>
-      <AppSidebar communities={communities} selectedAlias={alias} />
+      <AppSidebar
+        admin={admin}
+        communities={communities}
+        selectedAlias={alias}
+      />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
