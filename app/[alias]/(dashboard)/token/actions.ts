4e487ca44315edc3cec12628cae7d3482595ea68
sajee_techi <sajeewa@techi.be>
2025-03-21 13:00:47 +0530

    TODO in app/[alias]/(dashboard)/token/actions.ts

--- app/[alias]/(dashboard)/token/actions.ts
+++ app/[alias]/(dashboard)/token/actions.ts
@@ -6,8 +6,11 @@ import { BundlerService, CommunityConfig, Config } from '@citizenwallet/sdk';
 import { getAuthUserRoleInCommunityAction } from '@/app/_actions/admin-actions';
 import { Wallet } from 'ethers';
 import { mintTokenFormSchema } from './mint/form-schema';
+import { burnTokenFormSchema } from './burn/form-schema';
 import { z } from 'zod';
-export const searchMemberToMint = async (args: {
+
+// TODO: pass config as argument
+export const searchMember = async (args: {
   chainId: number;
   profileContract: string;
   query: string;
@@ -86,5 +89,66 @@ export const mintTokenToMemberAction = async (args: {
       throw error; // Rethrow the original error object
     }
     throw new Error('Failed to mint tokens'); // Fallback error message for non-Error objects
+  }
+};
+
+
+export const burnTokenFromMemberAction = async (args: {
+  config: Config;
+  formData: z.infer<typeof burnTokenFormSchema>;
+}) => {
+  const { config, formData } = args;
+  const { chain_id: chainId, address: tokenAddress } =
+    config.community.primary_token;
+  const communityConfig = new CommunityConfig(config);
+  const bundlerService = new BundlerService(communityConfig);
+
+  const result = burnTokenFormSchema.safeParse(formData);
+
+  if (!result.success) {
+    console.error(result.error);
+    throw new Error('Invalid form data');
+  }
+
+  const authRole = await getAuthUserRoleInCommunityAction({
+    alias: config.community.alias,
+    chainId: config.community.primary_token.chain_id
+  });
+
+  if (authRole !== 'owner') {
+    throw new Error('You are not authorized to burn tokens');
+  }
+
+  const serverWalletPrivateKey =
+    process.env[`SERVER_${chainId}_WALLET_PRIVATE_KEY`] ?? '';
+
+  if (!serverWalletPrivateKey) {
+    throw new Error('Signer cannot be found');
+  }
+
+  const wallet = new Wallet(serverWalletPrivateKey);
+
+  const {
+    member: { account: to },
+    amount,
+    description
+  } = formData;
+  const from = process.env[`SERVER_${chainId}_ACCOUNT_ADDRESS`] ?? '';
+
+  try {
+    await bundlerService.burnFromERC20Token(
+      wallet,
+      tokenAddress,
+      from,
+      to,
+      amount,
+      description
+    );
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      throw error; // Rethrow the original error object
+    }
+    throw new Error('Failed to burn tokens'); // Fallback error message for non-Error objects
   }
 };
