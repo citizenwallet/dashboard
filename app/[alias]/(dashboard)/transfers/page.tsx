f5ad2e7f81de87de31f1eb5416a6f74dce3045a3
sajee_techi <sajeewa@techi.be>
2025-03-24 17:16:17 +0530

    remove datepicker outside suspense border

--- app/[alias]/(dashboard)/transfers/page.tsx
+++ app/[alias]/(dashboard)/transfers/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react';
 import TransferTable from './_table/transfers-table';
 import { skeletonColumns, placeholderData } from './_table/columns';
 import { DataTable } from '@/components/ui/data-table';
+import { DatePickerWithPresets } from '@/components/custom/date-picker-with-presets';
+import { fetchCommunityByAliasAction } from '@/app/_actions/community-actions';
 
 export default async function Page(props: {
   params: Promise<{ alias: string }>;
@@ -14,6 +16,7 @@ export default async function Page(props: {
   }>;
 }) {
   const { alias } = await props.params;
+  const { community: config } = await fetchCommunityByAliasAction(alias);
 
   const {
     query: queryParam,
@@ -28,34 +31,39 @@ export default async function Page(props: {
   const to = toParam;
   const datePreset = datePresetParam || 'all';
   return (
-    <Suspense
-      key={alias + query + page + from + to + datePreset}
-      fallback={<Fallback />}
-    >
-      <TransferTable
-        query={query}
-        page={parseInt(page)}
-        alias={alias}
-        from={from}
-        to={to}
-      />
-    </Suspense>
+    <div className="flex flex-1 w-full flex-col h-full">
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <div className="flex flex-col">
+          <h1 className="text-2xl font-bold">Members</h1>
+          <p className="text-sm text-gray-500">{config.community.name}</p>
+        </div>
+        <div className="flex justify-end">
+          <DatePickerWithPresets />
+        </div>
+      </div>
+
+      <Suspense
+        key={alias + query + page + from + to}
+        fallback={<Fallback />}
+      >
+        <TransferTable
+          query={query}
+          page={parseInt(page)}
+          alias={alias}
+          from={from}
+          to={to}
+          config={config}
+        />
+      </Suspense>
+    </div>
   );
 }
 
 function Fallback() {
   return (
-    <div className="flex flex-1 w-full flex-col h-full">
-      <div className="grid grid-cols-2 gap-4 mb-4">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold">Transfers</h1>
-        </div>
-      </div>
-
-      <div className="flex-1 overflow-hidden">
-        <div className="h-full overflow-y-auto rounded-md border">
-          <DataTable columns={skeletonColumns} data={placeholderData} />
-        </div>
+    <div className="flex-1 overflow-hidden">
+      <div className="h-full overflow-y-auto rounded-md border">
+        <DataTable columns={skeletonColumns} data={placeholderData} />
       </div>
     </div>
   );
