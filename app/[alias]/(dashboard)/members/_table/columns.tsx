aa6a751636f33379165a7b5efd571a22160fcdb3
kev-techi <kevin@techi.be>
2025-03-26 07:55:56 +0100

    add copiable account address in member table

--- app/[alias]/(dashboard)/members/_table/columns.tsx
+++ app/[alias]/(dashboard)/members/_table/columns.tsx
@@ -11,12 +11,48 @@ import {
   CommunityConfig
 } from '@citizenwallet/sdk';
 import { JsonRpcProvider } from 'ethers';
-import { X } from 'lucide-react';
+import { Copy, X } from 'lucide-react';
 import { Check } from 'lucide-react';
+
+const IDRow = ({ account }: { account: string }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(account);
+    setIsCopied(true);
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <div className="w-[120px] truncate">
+      <div
+        className="flex items-center gap-1 cursor-pointer hover:bg-muted rounded-md p-1"
+        onClick={copyToClipboard}
+      >
+        <span className="text-xs font-mono truncate">
+          {formatAddress(account)}
+        </span>
+        {isCopied ? (
+          <Check className="ml-1 h-3 w-3" />
+        ) : (
+          <Copy className="ml-1 h-3 w-3" />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export const createColumns = (
   communityConfig: CommunityConfig
 ): ColumnDef<MemberT>[] => [
+  {
+    header: 'ID',
+    accessorKey: 'id',
+    cell: ({ row }) => <IDRow account={row.original.account} />
+  },
   {
     header: 'Member',
     cell: ({ row }) => {
