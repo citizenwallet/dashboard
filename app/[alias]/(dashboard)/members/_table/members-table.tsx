e58f791a7126d8d0afea41411d557dfbd9ffe3aa
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-21 08:02:53 +0530

    Enhance address validation features

--- app/[alias]/(dashboard)/members/_table/members-table.tsx
+++ app/[alias]/(dashboard)/members/_table/members-table.tsx
@@ -43,7 +43,7 @@ export default async function MembersTable({
 
       <div className="flex">
 
-        <SwitcherButton />
+        <SwitcherButton config={config} />
 
         <UrlSearch />
       </div>
