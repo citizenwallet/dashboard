27d7689897f5b3c9e76bc0fd58b9cbd946a603a0
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-21 08:02:53 +0530

    Enhance address validation features

--- app/[alias]/(dashboard)/members/_table/switcher-button.tsx
+++ app/[alias]/(dashboard)/members/_table/switcher-button.tsx
@@ -1,26 +1,47 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
+import { Config } from '@citizenwallet/sdk'
+import { ethers } from 'ethers'
 import { Plus } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useDebounce } from 'use-debounce'
+import { checkAvailableAddressMember } from '../action'
 
-export default function SwitcherButton() {
+
+export default function SwitcherButton({ config }: { config: Config }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const router = useRouter();
 
 
-    const [showAllMembers, setShowAllMembers] = useState<boolean>(false)
+    const [showAllMembers, setShowAllMembers] = useState<boolean>(false);
+    const [isAddDialogOpen, setIsAddDialogOpen] = useState<boolean>(false);
+    const [validAddress, setValidAddress] = useState<boolean>(false);
+    const [isAvailable, setIsAvailable] = useState<boolean>(false);
+    const [address, setAddress] = useState<string>('');
+    const usebouncedAddress = useDebounce(address, 1000);
 
-
+    //get the showMembers from the search params
     useEffect(() => {
         const showMembers = searchParams.get('showMembers') === 'true';
         setShowAllMembers(showMembers)
     }, [searchParams])
 
 
+    //handle the show all members
     const handleShowAllMembers = async () => {
         const params = new URLSearchParams(searchParams);
         params.set('showMembers', showAllMembers ? 'false' : 'true');
@@ -29,13 +50,112 @@ export default function SwitcherButton() {
 
     }
 
+    //check if the address is valid
+    const checkValidAddress = (address: string) => {
+        return ethers.isAddress(address);
+    }
+
+    //check if the address is available
+    const isAvailableAddress = async (address: string) => {
+        const isAvailable = await checkAvailableAddressMember({
+            config: config,
+            address: address
+        });
+        return isAvailable;
+    }
+
+    //check if the address is Available
+    useEffect(() => {
+        const checkAddress = async () => {
+
+            const isValid = checkValidAddress(usebouncedAddress[0]);
+            if (isValid) {
+                setValidAddress(true)
+                const isAvailable = await isAvailableAddress(usebouncedAddress[0]);
+                if (isAvailable) {
+                    setIsAvailable(false)
+                } else {
+                    setIsAvailable(true)
+                }
+            } else {
+                setValidAddress(false)
+            }
+
+        }
+        checkAddress();
+    }, [usebouncedAddress])
+
+
     return (
         <div className="flex flex-col gap-2">
 
-            <Button className="w-fit">
-                <Plus size={16} />
-                Add Member
-            </Button>
+            <Dialog open={isAddDialogOpen} onOpenChange={setIsAddDialogOpen}>
+                <DialogTrigger asChild>
+                    <div className="flex justify-start mb-4">
+                        <Button >
+                            <Plus size={16} />
+                            Add  Member
+                        </Button>
+                    </div>
+                </DialogTrigger>
+
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Create profile</DialogTitle>
+                        <DialogDescription>
+                            Add a new profile
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        {!validAddress && (
+                            <p className='text-red-500'>Invalid address.please enter a valid address</p>
+                        )}
+                        <div className="grid gap-2">
+                            <label htmlFor="memberAccount" className="text-sm font-medium">
+                                Enter account address
+                            </label>
+                            <Input
+                                id="memberAccount"
+                                placeholder="Member Account"
+                                value={address}
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </div>
+
+
+                    </div>
+                    <DialogFooter>
+                        <Button
+                            variant="outline"
+                            onClick={() => {
+                                setIsAddDialogOpen(false)
+                                setAddress('')
+                            }}
+                        >
+                            cancel
+                        </Button>
+                        {validAddress && (
+                            isAvailable ? (
+                                <Button
+                                    className="mb-2 md:mb-0"
+                                >
+                                    <Plus size={16} />
+                                    Add
+                                </Button>
+                            ) : (
+                                <Button
+                                    className="mb-2 md:mb-0"
+                                    onClick={() => {
+                                        router.push(`members/${address}`);
+                                    }}
+                                >
+                                    Edit
+                                </Button>
+                            )
+                        )}
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
 
             <div className="flex items-center space-x-2 my-4">
                 <Switch id="airplane-mode" checked={showAllMembers} onCheckedChange={handleShowAllMembers} />
