ba446902eb3a8ed1d72abdfe63e3c59498ff08bd
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-25 16:29:23 +0530

    change route for edit and add the member

--- app/[alias]/(dashboard)/members/[account]/add/profile.tsx
+++ app/[alias]/(dashboard)/members/[account]/add/profile.tsx
@@ -6,39 +6,32 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
-import { MemberT } from '@/services/chain-db/members'
 import { CommunityConfig, Config, checkUsernameAvailability } from '@citizenwallet/sdk'
-import { PenLine, Save, Trash2, Upload, User } from "lucide-react"
+import { Upload, User } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { useEffect, useMemo, useRef, useState } from "react"
 import { toast } from "sonner"
 import { useDebounce } from 'use-debounce'
-import type { Profile } from "./action"
-import { deleteProfileAction, updateProfileAction, updateProfileImageAction } from "./action"
+import type { Profile } from "../action"
+import { updateProfileAction, updateProfileImageAction } from "../action"
 
 export default function Profile({
-    memberData,
-    hasAdminRole,
+
     config,
-    type,
     account
 }: {
-    memberData?: MemberT | null,
-    hasAdminRole: boolean,
+
+
     config: Config,
-    type: 'edit' | 'new',
     account: string
 }) {
     const community = useMemo(() => new CommunityConfig(config), [config]);
-    const [isEditing, setIsEditing] = useState(
-        type === 'edit' ? false : true
-    );
 
     const [userData, setUserData] = useState({
-        username: type === 'edit' ? memberData?.username : '',
-        name: type === 'edit' ? memberData?.name : '',
-        description: type === 'edit' ? memberData?.description : '',
-        avatarUrl: type === 'edit' ? memberData?.image : '',
+        username: '',
+        name: '',
+        description: '',
+        avatarUrl: '',
     });
 
     const [imageFile, setImageFile] = useState<File | null>(null);
@@ -49,17 +42,10 @@ export default function Profile({
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
 
-    const handleEdit = () => {
-        setIsEditing(true)
-    }
 
     useEffect(() => {
         if (debouncedUsername && usernameEdit) {
             const checkUsername = async () => {
-
-                if (debouncedUsername == memberData?.username) { // <-- username is taken by current user
-                    return;
-                }
 
                 try {
                     const isAvailable = await checkUsernameAvailability(community, debouncedUsername);
@@ -76,92 +62,8 @@ export default function Profile({
 
             checkUsername();
         }
-    }, [debouncedUsername, usernameEdit, community, memberData?.username]);
+    }, [debouncedUsername, usernameEdit, community]);
 
-    //handle the edit profile data saved
-    const handleSave = async () => {
-
-        try {
-            setIsLoading(true);
-            if (userData.username === memberData?.username &&
-                userData.name === memberData?.name &&
-                userData.description === memberData?.description &&
-                userData.avatarUrl === memberData?.image) {
-
-                toast.error('No changes to save');
-                setIsLoading(false);
-                return
-            }
-
-            if (!isAvailable) {
-                toast.error('Username is already taken,You can not save it');
-                setIsLoading(false);
-                return
-            }
-            //default image
-            let cid = memberData?.image;
-
-            if (userData.avatarUrl != memberData?.image) {
-
-                if (!imageFile) {
-                    toast.error('Please upload an image')
-                    setIsLoading(false);
-                    return
-                }
-
-                const response = await updateProfileImageAction(imageFile, community.community.alias);
-                cid = response.IpfsHash;
-            }
-
-            const profile: Profile = {
-                account: memberData?.account || '',
-                description: userData.description || "",
-                image: `ipfs://${cid}`,
-                image_medium: `ipfs://${cid}`,
-                image_small: `ipfs://${cid}`,
-                name: userData.name || "",
-                username: userData.username || "",
-            };
-
-            await updateProfileAction(profile, config.community.alias, config);
-            toast.success('Profile updated successfully');
-
-        } catch (error) {
-            console.error('Error updating profile:', error);
-            toast.error('Error updating profile');
-        } finally {
-            setIsEditing(false);
-            setIsLoading(false);
-        }
-
-
-    }
-
-    //handle the delete profile
-    const handleDelete = () => {
-        toast.custom((t) => (
-            <div className="flex flex-col gap-2 bg-background p-2 rounded-lg border">
-                <h3 className="font-semibold text-base">Delete Profile</h3>
-                <p className="text-sm text-muted-foreground">
-                    This will permanently delete this member profile.
-                </p>
-                <div className="flex gap-2 justify-end pt-2">
-                    <Button variant="outline" onClick={() => toast.dismiss(t)}>Cancel</Button>
-                    <Button
-                        variant="destructive"
-                        onClick={async () => {
-                            await deleteProfileAction(
-                                userData.avatarUrl || '', config.community.alias, config, memberData?.account || ''
-                            );
-                            toast.success('Profile deleted successfully');
-                        }}
-                    >
-                        Delete
-                    </Button>
-                </div>
-            </div>
-        ));
-    };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { id, value } = e.target
@@ -232,13 +134,14 @@ export default function Profile({
                 image_medium: `ipfs://${cid}`,
                 image_small: `ipfs://${cid}`,
                 name: userData.name || "",
-                username: userData.username || "",
+                username: userData.username,
             };
 
-
+            console.log(profile);
             await updateProfileAction(profile, config.community.alias, config);
             toast.success('Profile updated successfully');
 
+            //call to API to add member
 
         } catch (error) {
             console.error('Error adding member:', error);
@@ -261,9 +164,7 @@ export default function Profile({
                                 <User className="h-12 w-12" />
                             </AvatarFallback>
                         </Avatar>
-                        {type === 'edit' && (
-                            <p className="text-sm text-gray-500">@{memberData?.username}</p>
-                        )}
+
                         <input
                             type="file"
                             ref={fileInputRef}
@@ -275,7 +176,6 @@ export default function Profile({
                             variant="outline"
                             size="sm"
                             className="text-xs"
-                            disabled={!isEditing}
                             onClick={triggerFileInput}
                         >
                             <Upload className="mr-2 h-3 w-3" />
@@ -292,7 +192,6 @@ export default function Profile({
                                     placeholder="Username"
                                     value={userData.username}
                                     onChange={handleChange}
-                                    disabled={!isEditing}
                                     className={`bg-white ${isAvailable ? '' : 'border-red-500'}`}
                                 />
                             </div>
@@ -304,7 +203,6 @@ export default function Profile({
                                     placeholder="Your name"
                                     value={userData.name}
                                     onChange={handleChange}
-                                    disabled={!isEditing}
                                     className="bg-white"
                                 />
                             </div>
@@ -317,7 +215,6 @@ export default function Profile({
                                 placeholder="Tell us about yourself"
                                 value={userData.description}
                                 onChange={handleChange}
-                                disabled={!isEditing}
                                 className="min-h-[120px] bg-white resize-none"
                             />
                         </div>
@@ -325,61 +222,18 @@ export default function Profile({
                 </div>
             </CardContent>
 
-            {/* it can access only admin and community owner  */}
-            {hasAdminRole && type === 'edit' && (
-                <CardFooter className="flex justify-between pt-6">
-                    {isEditing ? (
-                        <div className="flex gap-3">
-                            <Button
-                                onClick={handleSave}
-                                className="gap-2"
-                                disabled={!isAvailable}
-                            >
-                                <Save className="h-4 w-4" />
-                                {isLoading ? 'Saving...' : 'Save Changes'}
-                            </Button>
-                            <Button variant="outline" onClick={() => {
-                                setIsEditing(false);
-                                setUsernameEdit(false);
-                                setUserData({
-                                    username: memberData?.username || "",
-                                    name: memberData?.name || "",
-                                    description: memberData?.description || "",
-                                    avatarUrl: memberData?.image || "",
-                                })
-                                setIsAvailable(true);
-                            }}>
-                                Cancel
-                            </Button>
-                        </div>
-                    ) : (
-                        <Button onClick={handleEdit} variant="outline" className="gap-2">
-                            <PenLine className="h-4 w-4" />
-                            Edit Profile
-                        </Button>
-                    )}
 
-                    <Button variant="destructive" onClick={handleDelete} className="gap-2">
-                        <Trash2 className="h-4 w-4" />
-                        Delete Account
-                    </Button>
-                </CardFooter>
-            )}
+            <CardFooter className="flex justify-between pt-6">
+                <Button variant="destructive" className="gap-2" onClick={() => {
+                    router.push(`/${config.community.alias}/members`);
+                }}>
+                    Cancel
+                </Button>
 
-            {/* if the type is new, show the cancel and add member button */}
-            {type === 'new' && (
-                <CardFooter className="flex justify-between pt-6">
-                    <Button variant="destructive" className="gap-2" onClick={() => {
-                        router.push(`/${config.community.alias}/members`);
-                    }}>
-                        Cancel
-                    </Button>
-
-                    <Button variant="outline" className="gap-2" disabled={!isAvailable} onClick={handleAddMember}>
-                        {isLoading ? 'Saving...' : 'Add Member'}
-                    </Button>
-                </CardFooter>
-            )}
+                <Button variant="outline" className="gap-2" disabled={!isAvailable} onClick={handleAddMember}>
+                    {isLoading ? 'Saving...' : 'Add Member'}
+                </Button>
+            </CardFooter>
 
         </Card>
 
