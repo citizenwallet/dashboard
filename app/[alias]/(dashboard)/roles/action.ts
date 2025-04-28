3e4fa73773bf7c8ed689e4219fff36298a2220cc
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-28 16:51:07 +0530

    Add revokeRoleAction function and enhance transaction success handling for role management

--- app/[alias]/(dashboard)/roles/action.ts
+++ app/[alias]/(dashboard)/roles/action.ts
@@ -4,7 +4,8 @@ import {
   BundlerService,
   CommunityConfig,
   Config,
-  MINTER_ROLE
+  MINTER_ROLE,
+  waitForTxSuccess
 } from '@citizenwallet/sdk';
 import { Wallet } from 'ethers';
 
@@ -28,6 +29,38 @@ export const grantRoleAction = async (args: {
     MINTER_ROLE,
     account
   );
-  console.log('hash', hash);
-  return hash;
+  const isSuccess = await waitForTxSuccess(community, hash);
+  if (isSuccess) {
+    return { success: true };
+  } else {
+    return { success: false };
+  }
+};
+
+export const revokeRoleAction = async (args: {
+  config: Config;
+  account: string;
+}): Promise<{ success: boolean }> => {
+  const { config, account } = args;
+  const community = new CommunityConfig(config);
+  const bundler = new BundlerService(community);
+
+  const tokenAddress = community.primaryToken.address;
+
+  const signer = new Wallet(process.env.SERVER_PRIVATE_KEY as string);
+  const signerAccountAddress = process.env.SERVER_ACCOUNT_ADDRESS as string;
+
+  const hash = await bundler.revokeRole(
+    signer,
+    tokenAddress,
+    signerAccountAddress,
+    MINTER_ROLE,
+    account
+  );
+  const isSuccess = await waitForTxSuccess(community, hash);
+  if (isSuccess) {
+    return { success: true };
+  } else {
+    return { success: false };
+  }
 };
