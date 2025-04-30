5caffc8258316fd2d10b1252f654b35b2454f3d6
Randika Dilshan <rdilshan077788@gmail.com>
2025-04-30 17:49:25 +0530

    getEvents function

---
+++ services/chain-db/event.ts
@@ -0,0 +1,24 @@
+import 'server-only';
+
+import { PostgrestResponse, SupabaseClient } from '@supabase/supabase-js';
+
+const TABLE_NAME = 't_events_';
+
+export interface Event {
+  id: string;
+  name: string;
+  contract: string;
+  event_signature: string;
+  topic: string;
+  alias: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export const getEvents = async (args: {
+  client: SupabaseClient;
+  chainId: string;
+}): Promise<PostgrestResponse<Event>> => {
+  const { client, chainId } = args;
+  return client.from(TABLE_NAME + chainId).select(`*`);
+};
