0a17ed3caf6ce30b3d9a1619e83a8af6605fc5a6
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-02 09:52:58 +0530

    Refactor and extend event retrieval functions to include getEventByContractAndTopic

--- services/chain-db/event.ts
+++ services/chain-db/event.ts
@@ -1,6 +1,10 @@
 import 'server-only';
 
-import { PostgrestResponse, SupabaseClient } from '@supabase/supabase-js';
+import {
+  PostgrestResponse,
+  PostgrestSingleResponse,
+  SupabaseClient
+} from '@supabase/supabase-js';
 
 const TABLE_NAME = 't_events_';
 
@@ -15,11 +19,31 @@ export interface Event {
   updated_at: Date;
 }
 
-export const getEvents = async (args: {
+export const getEventsByChainId = async (args: {
   client: SupabaseClient;
   chainId: string;
   alias: string;
 }): Promise<PostgrestResponse<Event>> => {
   const { client, chainId, alias } = args;
-  return client.from(TABLE_NAME + chainId).select(`*`).eq('alias', alias);
+  return client
+    .from(TABLE_NAME + chainId)
+    .select(`*`)
+    .eq('alias', alias);
+};
+
+export const getEventByContractAndTopic = async (args: {
+  client: SupabaseClient;
+  chainId: string;
+  contract: string;
+  topic?: string | null;
+  alias: string;
+}): Promise<PostgrestSingleResponse<Event>> => {
+  const { client, chainId, contract, topic, alias } = args;
+  return client
+    .from(TABLE_NAME + chainId)
+    .select(`*`)
+    .eq('contract', contract)
+    .eq('topic', topic)
+    .eq('alias', alias)
+    .single();
 };
