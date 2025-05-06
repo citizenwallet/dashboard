a27f79e8ac304ce4e7f93e53fa59eb94a9ee913a
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-06 16:46:12 +0530

    fix condition for username availability check in profile edit

--- app/[alias]/(dashboard)/members/[account]/edit/profile.tsx
+++ app/[alias]/(dashboard)/members/[account]/edit/profile.tsx
@@ -80,7 +80,7 @@ export default function Profile({
   };
 
   useEffect(() => {
-    if (debouncedUsername && (usernameEdit || isEditing)) {
+    if (debouncedUsername && (usernameEdit && isEditing)) {
       const checkUsername = async () => {
         if (debouncedUsername === memberData?.username) {
           setIsAvailable(true);
@@ -199,7 +199,7 @@ export default function Profile({
                 }
               });
             }}
-                >
+          >
             {isLoading ? 'Deleting...' : 'Delete'}
           </Button>
         </div>
