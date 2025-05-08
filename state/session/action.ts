83e14fe53e6bb9ff0267964c5feab9c0b73231cd
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-08 10:56:28 +0530

    Add session management logic with Zustand for state handling and storage integration

---
+++ state/session/action.ts
@@ -0,0 +1,89 @@
+import { StorageService } from '@/services/storage';
+import * as cwSDK from '@citizenwallet/sdk';
+import { Wallet } from 'ethers';
+import { SessionState } from './state';
+
+export class SessionLogic {
+  state: SessionState;
+  communityConfig: cwSDK.CommunityConfig;
+  storage: StorageService;
+
+  constructor(state: SessionState, config: cwSDK.Config) {
+    this.state = state;
+    this.communityConfig = new cwSDK.CommunityConfig(config);
+    this.storage = new StorageService(this.communityConfig.community.alias);
+  }
+
+  storePrivateKey(privateKey: string) {
+    this.storage.setKey('session_private_key', privateKey);
+    this.state.setPrivateKey(privateKey);
+  }
+
+  storeSessionHash(hash: string) {
+    this.storage.setKey('session_hash', hash);
+    this.state.setHash(hash);
+  }
+
+  storeSourceValue(sourceValue: string) {
+    this.storage.setKey('session_source_value', sourceValue);
+    this.state.setSourceValue(sourceValue);
+  }
+
+  storeSourceType(sourceType: string) {
+    this.storage.setKey('session_source_type', sourceType);
+    this.state.setSourceType(sourceType);
+  }
+
+  async getAccountAddress() {
+    const sourceValue =
+      this.state.sourceValue || this.storage.getKey('session_source_value');
+
+    if (!sourceValue) {
+      throw new Error('Source value not found');
+    }
+
+    const sourceType =
+      this.state.sourceType || this.storage.getKey('session_source_type');
+
+    if (!sourceType) {
+      throw new Error('Source type not found');
+    }
+
+    const accountAddress = await cwSDK.getTwoFAAddress({
+      community: this.communityConfig,
+      source: sourceValue,
+      type: sourceType
+    });
+
+    return accountAddress;
+  }
+
+  async isSessionExpired() {
+    const accountAddress = await this.getAccountAddress();
+
+    if (!accountAddress) {
+      throw new Error('Account address not found');
+    }
+
+    const privateKey =
+      this.state.privateKey || this.storage.getKey('session_private_key');
+
+    if (!privateKey) {
+      throw new Error('Private key not found');
+    }
+
+    const signer = new Wallet(privateKey);
+
+    const isExpired = await cwSDK.isSessionExpired({
+      community: this.communityConfig,
+      account: accountAddress,
+      owner: signer.address
+    });
+
+    return isExpired;
+  }
+
+  clear() {
+    this.state.clear();
+  }
+}
