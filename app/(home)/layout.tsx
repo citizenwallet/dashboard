ca0bf53cb3308f4e20f46825f2c3cfdd116887bf
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-14 15:09:00 +0530

    Refactor layout component to remove user authentication and redirection logic

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -9,36 +9,12 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const lastViewedAlias = (await cookies()).get('lastViewedAlias')?.value;
-  const user = await getAuthUserAction();
-
-  if (!user) {
-    redirect('/login');
-  }
-
-  const { role } = user;
-  const accessList =
-    user?.users_community_access.map((access) => access.alias) ?? [];
-
-  if (
-    role === 'user' &&
-    lastViewedAlias &&
-    accessList.includes(lastViewedAlias)
-  ) {
-    redirect(`/${lastViewedAlias}`);
-  }
-
-  if (role === 'user' && accessList.length > 0) {
-    const alias = accessList[0];
-    redirect(`/${alias}`);
-  }
 
   return (
     <main className="flex min-h-screen w-full flex-col">
       <div className="flex flex-col h-screen">
         <header className="flex h-14 items-center gap-4 border-b bg-background px-4 sm:px-6">
           <UrlSearch />
-          <User user={user} />
         </header>
         <main className="flex-1 overflow-hidden p-4 sm:px-6">{children}</main>
       </div>
