e809ea21212166c130664b45fedea8d5ace71de9
Randika Dilshan <rdilshan077788@gmail.com>
2025-05-31 09:35:13 +0530

    Add user role validation to uploadItemImageAction and updateProfileAction; ensure authenticated access

--- app/[alias]/(dashboard)/profile/action.ts
+++ app/[alias]/(dashboard)/profile/action.ts
@@ -1,10 +1,31 @@
 'use server';
 
+import {
+  getAuthUserRoleInAppAction,
+  getAuthUserRoleInCommunityAction
+} from '@/app/_actions/user-actions';
 import { uploadImage } from '@/services/storage';
 import { getServiceRoleClient } from '@/services/top-db';
+import { updateCommunityJson } from '@/services/top-db/community';
+import { Config } from '@citizenwallet/sdk';
+import { revalidatePath } from 'next/cache';
 
 export async function uploadItemImageAction(imageFile: File, alias: string) {
   const client = getServiceRoleClient();
+
+  const roleInCommunity = await getAuthUserRoleInCommunityAction({
+    alias
+  });
+
+  const roleInApp = await getAuthUserRoleInAppAction();
+
+  if (!roleInApp) {
+    throw new Error('Unauthenticated user');
+  }
+
+  if (roleInApp === 'user' && !roleInCommunity) {
+    throw new Error('You are not a member of this community');
+  }
 
   try {
     // Upload the image to storage
@@ -14,5 +35,41 @@ export async function uploadItemImageAction(imageFile: File, alias: string) {
   } catch (error) {
     console.error('Error uploading image:', error);
     throw new Error('Failed to upload image');
+  }
+}
+
+export async function updateProfileAction(data: Config, alias: string) {
+  const client = getServiceRoleClient();
+
+  const roleInCommunity = await getAuthUserRoleInCommunityAction({
+    alias
+  });
+
+  const roleInApp = await getAuthUserRoleInAppAction();
+
+  if (!roleInApp) {
+    throw new Error('Unauthenticated user');
+  }
+
+  if (roleInApp === 'user' && !roleInCommunity) {
+    throw new Error('You are not a member of this community');
+  }
+
+  try {
+    // Update the profile in the database
+    const { data: updatedData, error } = await updateCommunityJson(
+      client,
+      alias,
+      data
+    );
+
+    if (error) {
+      throw new Error('Failed to update profile');
+    }
+    revalidatePath(`/${alias}/profile`, 'page');
+    return updatedData;
+  } catch (error) {
+    console.error('Error updating profile:', error);
+    throw new Error('Failed to update profile');
   }
 }
