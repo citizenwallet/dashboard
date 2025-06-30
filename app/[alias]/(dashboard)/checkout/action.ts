b69d43ae24679309cefb4eb2562c63c6bb0c4ec2
Randika Dilshan <rdilshan077788@gmail.com>
2025-06-30 16:01:23 +0530

    Add deployPaymasterAction function , including initialization with whitelisted addresses .

--- app/[alias]/(dashboard)/checkout/action.ts
+++ app/[alias]/(dashboard)/checkout/action.ts
@@ -6,6 +6,10 @@ import {
   ERC1967_BYTECODE
 } from './contract/ERC1967Proxy_contract';
 import { PROFILE_ABI, PROFILE_BYTECODE } from './contract/profile_contract';
+import {
+  PAYMASTER_ABI,
+  PAYMASTER_BYTECODE
+} from './contract/paymaster_contract';
 
 interface ProxyDeployParams {
   initializeArgs?: string[];
@@ -87,5 +91,67 @@ export async function deployProfileAction({
     };
   } catch (error) {
     console.error('Proxy deployment error:', error);
+  }
+}
+
+export async function deployPaymasterAction({
+  privateKey,
+  chainId,
+  profileAddress,
+  tokenAddress
+}: {
+  privateKey: string;
+  chainId: string;
+  profileAddress: string;
+  tokenAddress: string;
+}) {
+  try {
+    // Get the provider from environment variable
+    const provider = new ethers.JsonRpcProvider(CHAIN_ID_TO_RPC_URL(chainId));
+
+    // Create a wallet instance
+    const wallet = new ethers.Wallet(privateKey, provider);
+
+    // Create contract factory
+    const factory = new ethers.ContractFactory(
+      PAYMASTER_ABI,
+      PAYMASTER_BYTECODE,
+      wallet
+    );
+
+    // Deploy the contract with constructor arguments if provided
+    const contract = await factory.deploy({
+      gasLimit: 3000000
+    });
+
+    // Wait for deployment to complete
+    await contract.waitForDeployment();
+
+    // Get the contract address
+    const contractAddress = await contract.getAddress();
+
+    // Create a new contract instance for interaction
+    const paymasterContract = new ethers.Contract(
+      contractAddress,
+      PAYMASTER_ABI,
+      wallet
+    );
+
+    // whitelisted addresses
+    const whitelistedAddresses = [
+      profileAddress,
+      tokenAddress,
+      '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28', // Card Manager
+      '0xE2F3DC3E638113b9496060349e5332963d9C1152' // Session Manager
+    ];
+
+    // Initialize the contract with the deployer as sponsor and whitelisted addresses
+    await paymasterContract.initialize(wallet.address, whitelistedAddresses, {
+      gasLimit: 3000000
+    });
+
+    return contractAddress;
+  } catch (error) {
+    console.error('Contract deployment error:', error);
   }
 }
