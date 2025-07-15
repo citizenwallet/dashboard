e721fae545ca22ed579b445d19fd44a4f3ad3b7e
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-15 07:29:49 +0530

    Refactor Paymaster page to use async data loading and introduce fallback component and update PaymasterTable to accept initial data

--- app/[alias]/(dashboard)/paymaster/page.tsx
+++ app/[alias]/(dashboard)/paymaster/page.tsx
@@ -1,6 +1,22 @@
+import { getServiceRoleClient } from '@/services/chain-db';
+import { getPaymasterByAlias } from "@/services/chain-db/paymaster";
+import { getCommunity } from "@/services/cw";
+import { Config } from "@citizenwallet/sdk";
+import { Suspense } from "react";
+import PaymasterFallback from "./_components/fallback";
 import PaymasterTable from "./paymaster-table";
 
-export default function page() {
+export default async function page(props: {
+    params: Promise<{ alias: string }>;
+    searchParams: Promise<{
+        page?: string;
+    }>;
+}) {
+    const { alias } = await props.params;
+    const { community: config } = await getCommunity(alias);
+    const { page: pageParam } = await props.searchParams;
+    const page = pageParam || '1';
+
     return (
         <div className="flex flex-1 w-full flex-col h-full">
             <div className="grid grid-cols-2 gap-4">
@@ -8,9 +24,32 @@ export default function page() {
                     <h1 className="text-2xl font-bold">Paymaster</h1>
                     <p className="text-sm text-gray-500"> update paymaster </p>
                 </div>
-
             </div>
-            <PaymasterTable />
+            <Suspense fallback={<PaymasterFallback />} key={alias + config.community.alias + page}>
+                <PageLoader config={config} page={page} />
+            </Suspense>
         </div>
+    )
+}
+
+async function PageLoader({
+    config,
+    page
+}: {
+    config: Config;
+    page?: string;
+}) {
+    const { chain_id: chainId } = config.community.profile;
+
+    const supabase = getServiceRoleClient(chainId);
+
+    const { data: paymasterData } = await getPaymasterByAlias({
+        client: supabase,
+        alias: config.community.alias,
+        page: parseInt(page || '1')
+    });
+
+    return (
+        <PaymasterTable initialData={paymasterData || []} />
     )
 }
