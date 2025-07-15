8340cc5752cf285d9ffdf296e10469a5513ec08b
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-15 15:43:23 +0530

    add the fallback

--- app/[alias]/(dashboard)/paymaster/page.tsx
+++ app/[alias]/(dashboard)/paymaster/page.tsx
@@ -5,6 +5,7 @@ import { getCommunity } from "@/services/cw";
 import { Suspense } from "react";
 import { placeholderData, skeletonColumns } from "./_components/fallback";
 import PaymasterTable from "./paymaster-table";
+import { Config } from '@citizenwallet/sdk';
 
 export default async function page(props: {
     params: Promise<{ alias: string }>;
@@ -17,6 +18,29 @@ export default async function page(props: {
     const { page: pageParam } = await props.searchParams;
     const page = pageParam || '1';
 
+    return (
+        <div className="flex flex-1 w-full flex-col h-full">
+            <div className="grid grid-cols-2 gap-4">
+                <div className="flex flex-col">
+                    <h1 className="text-2xl font-bold">Paymaster</h1>
+                    <p className="text-sm text-gray-500"> update whitelist on paymaster contract </p>
+                </div>
+            </div>
+            <Suspense fallback={<Fallback />} >
+                <PageLoader config={config} page={page} />
+            </Suspense>
+        </div>
+    )
+}
+
+
+async function PageLoader({
+    config,
+    page
+}: {
+    config: Config,
+    page: string;
+}) {
 
     const { chain_id: chainId } = config.community.profile;
 
@@ -28,28 +52,25 @@ export default async function page(props: {
         page: parseInt(page)
     });
 
+
     return (
-        <div className="flex flex-1 w-full flex-col h-full">
-            <div className="grid grid-cols-2 gap-4">
-                <div className="flex flex-col">
-                    <h1 className="text-2xl font-bold">Paymaster</h1>
-                    <p className="text-sm text-gray-500"> update paymaster </p>
-                </div>
-            </div>
-            <Suspense fallback={<Fallback />} key={paymasterData?.length}>
-                <PaymasterTable initialData={paymasterData || []} config={config} />
-            </Suspense>
-        </div>
+        <PaymasterTable
+            initialData={paymasterData || []}
+            config={config}
+        />
     )
 }
 
 
+
 function Fallback() {
     return (
-        <div className="flex-1 overflow-hidden">
-            <div className="h-full overflow-y-auto rounded-md border">
-                <DataTable columns={skeletonColumns} data={placeholderData} />
+        <>
+            <div className="flex-1 overflow-hidden">
+                <div className="h-full overflow-y-auto rounded-md border">
+                    <DataTable columns={skeletonColumns} data={placeholderData} />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
