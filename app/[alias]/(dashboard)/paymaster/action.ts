a0838e09e0f729e953478b78428718af43110cf7
Randika Dilshan <rdilshan077788@gmail.com>
2025-07-15 15:31:30 +0530

    Add refreshPaymasterWhitelistAction to handle updating the paymaster whitelist

--- app/[alias]/(dashboard)/paymaster/action.ts
+++ app/[alias]/(dashboard)/paymaster/action.ts
@@ -7,10 +7,29 @@ import {
 import { getServiceRoleClient } from '@/services/chain-db';
 import {
   getPaymasterByAddress,
-  updatePaymasterName
+  Paymaster,
+  updatePaymasterName,
+  upsertPaymasterWhitelist
 } from '@/services/chain-db/paymaster';
 import { CommunityConfig, Config } from '@citizenwallet/sdk';
+import { ethers } from 'ethers';
 import { revalidatePath } from 'next/cache';
+import { PAYMASTER_ABI } from './contract/paymaster_contract';
+
+const CHAIN_ID_TO_RPC_URL = (chainId: string) => {
+  switch (chainId) {
+    case '137':
+      return process.env.POLYGON_RPC_URL;
+    case '100':
+      return process.env.GNOSIS_RPC_URL;
+    case '42220':
+      return process.env.CELO_RPC_URL;
+    case '42161':
+      return process.env.ARBITRUM_RPC_URL;
+    default:
+      return process.env.BASE_RPC_URL;
+  }
+};
 
 export const updatePaymasterNameAction = async (args: {
   config: Config;
@@ -71,4 +90,69 @@ export const checkPaymasterWhitelistAddressExistsAction = async (args: {
     address: address,
     alias: alias
   });
+};
+
+export const refreshPaymasterWhitelistAction = async (args: {
+  config: Config;
+  data: Paymaster[];
+}) => {
+  const { config, data } = args;
+
+  try {
+    const communityConfig = new CommunityConfig(config);
+    const chainId = communityConfig.primaryToken.chain_id;
+    const paymasteraddress =
+      communityConfig.primaryAccountConfig.paymaster_address;
+    const alias = communityConfig.community.alias;
+
+    const roleInApp = await getAuthUserRoleInAppAction();
+    const roleResult = await getAuthUserRoleInCommunityAction({ alias });
+
+    if (roleInApp != 'admin' && roleResult != 'owner') {
+      throw new Error('You are not authorized to upload paymaster whitelist');
+    }
+
+    if (!process.env.WHITELIST_ACCOUNT_WALLET_PRIVATE_KEY) {
+      throw new Error('WHITELIST_ACCOUNT_WALLET_PRIVATE_KEY is not set');
+    }
+
+    const provider = new ethers.JsonRpcProvider(
+      CHAIN_ID_TO_RPC_URL(chainId.toString())
+    );
+
+    const wallet = new ethers.Wallet(
+      process.env.WHITELIST_ACCOUNT_WALLET_PRIVATE_KEY,
+      provider
+    );
+
+    const paymasterContract = new ethers.Contract(
+      paymasteraddress,
+      PAYMASTER_ABI,
+      wallet
+    );
+
+    const whitelist = data.map((item) => item.contract);
+
+    const tx = await paymasterContract.updateWhitelist(whitelist);
+
+    const receipt = await tx.wait();
+    const txHash = receipt.hash;
+
+    const supabase = getServiceRoleClient(chainId);
+    const newdata = data.map((item) => ({
+      ...item,
+      published: txHash
+    }));
+
+    await upsertPaymasterWhitelist({
+      client: supabase,
+      data: newdata
+    });
+
+    revalidatePath(`/${alias}/paymaster`, 'page');
+    return { success: true };
+  } catch (error) {
+    console.error(error);
+    return { success: false };
+  }
 };
