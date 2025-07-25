888f7773b0992379f1044b8a6c0143b24fe73a87
sajee_techi <sajeewa@techi.be>
2025-07-25 13:53:41 +0530

    add sessions to newly created communities

--- lib/address.ts
+++ lib/address.ts
@@ -3,3 +3,6 @@ export const primaryCardManager =
   
 export const primarySessionManager=
   '0xE2F3DC3E638113b9496060349e5332963d9C1152';
+
+export const sessionFactoryAddress = '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9';
+export const sessionProviderAddress = '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9';
