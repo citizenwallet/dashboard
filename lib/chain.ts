0d2f30826344489be6b840e1ea0f5e095ab6984d
sajee_techi <sajeewa@techi.be>
2025-07-25 17:17:59 +0530

    chain level - primary account factory - entry point address - primary card manager - session module address - session factory address - session provider address

--- lib/chain.ts
+++ lib/chain.ts
@@ -5,6 +5,10 @@ export type ChainOption = {
   rpcUrl: string;
   primaryAccountFactory: string;
   entrypointAddress: string;
+  primaryCardManager: string;
+  sessionModuleAddress: string;
+  sessionFactoryAddress: string;
+  sessionProviderAddress: string;
 };
 
 export const mainnetChains: ChainOption[] = [
@@ -13,8 +17,15 @@ export const mainnetChains: ChainOption[] = [
     name: 'Gnosis',
     logo: '/chainLogo/Gnosis.png',
     rpcUrl: process.env.GNOSIS_RPC_URL || '',
+
     primaryAccountFactory: '0xBABCf159c4e3186cf48e4a48bC0AeC17CF9d90FE',
-    entrypointAddress: '0xAAEb9DC18aDadae9b3aE7ec2b47842565A81113f'
+    entrypointAddress: '0xAAEb9DC18aDadae9b3aE7ec2b47842565A81113f',
+
+    primaryCardManager: '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28',
+
+    sessionModuleAddress: '0xE2F3DC3E638113b9496060349e5332963d9C1152',
+    sessionFactoryAddress: '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9',
+    sessionProviderAddress: '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9'
   },
   {
     id: '42220',
@@ -22,7 +33,11 @@ export const mainnetChains: ChainOption[] = [
     logo: '/chainLogo/Celo.png',
     rpcUrl: process.env.CELO_RPC_URL || '',
     primaryAccountFactory: '0xcd8b1B9E760148c5026Bc5B0D56a5374e301FDcA',
-    entrypointAddress: '0x66fE9c22CcA49B257dd4F00508AC90198d99Bf27'
+    entrypointAddress: '0x66fE9c22CcA49B257dd4F00508AC90198d99Bf27',
+    primaryCardManager: '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28',
+    sessionModuleAddress: '0xE2F3DC3E638113b9496060349e5332963d9C1152',
+    sessionFactoryAddress: '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9',
+    sessionProviderAddress: '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9'
   },
   {
     id: '42161',
@@ -30,7 +45,11 @@ export const mainnetChains: ChainOption[] = [
     logo: '/chainLogo/Arbitrum.png',
     rpcUrl: process.env.ARBITRUM_RPC_URL || '',
     primaryAccountFactory: '0x0000000000000000000000000000000000000000',
-    entrypointAddress: '0x0000000000000000000000000000000000000000'
+    entrypointAddress: '0x0000000000000000000000000000000000000000',
+    primaryCardManager: '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28',
+    sessionModuleAddress: '0xE2F3DC3E638113b9496060349e5332963d9C1152',
+    sessionFactoryAddress: '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9',
+    sessionProviderAddress: '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9'
   },
   {
     id: '137',
@@ -38,15 +57,26 @@ export const mainnetChains: ChainOption[] = [
     logo: '/chainLogo/Polygon.png',
     rpcUrl: process.env.POLYGON_RPC_URL || '',
     primaryAccountFactory: '0x940Cbb155161dc0C4aade27a4826a16Ed8ca0cb2',
-    entrypointAddress: '0x7079253c0358eF9Fd87E16488299Ef6e06F403B6'
+    entrypointAddress: '0x7079253c0358eF9Fd87E16488299Ef6e06F403B6',
+    primaryCardManager: '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28',
+    sessionModuleAddress: '0xE2F3DC3E638113b9496060349e5332963d9C1152',
+    sessionFactoryAddress: '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9',
+    sessionProviderAddress: '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9'
   },
   {
     id: '8453',
     name: 'Base',
     logo: '/chainLogo/Base.png',
     rpcUrl: process.env.BASE_RPC_URL || '',
+
     primaryAccountFactory: '0x05e2Fb34b4548990F96B3ba422eA3EF49D5dAa99',
-    entrypointAddress: '0xAE76B1C6818c1DD81E20ccefD3e72B773068ABc9'
+    entrypointAddress: '0xAE76B1C6818c1DD81E20ccefD3e72B773068ABc9',
+
+    primaryCardManager: '0xBA861e2DABd8316cf11Ae7CdA101d110CF581f28',
+
+    sessionModuleAddress: '0xE2F3DC3E638113b9496060349e5332963d9C1152',
+    sessionFactoryAddress: '0xEd0cD3886b84369A0e29Db9a4480ADF5051c76C9',
+    sessionProviderAddress: '0xF3004A1690f97Cf5d307eDc5958a7F76b62f9FC9'
   }
 ];
 
@@ -56,9 +86,15 @@ export const testnetChains: ChainOption[] = [
     name: 'Gnosis Testnet',
     logo: '/chainLogo/Gnosis.png',
     rpcUrl: process.env.GNOSIS_CHIADO_RPC_URL || '',
-    // TODO: add chiado values
-    primaryAccountFactory: '0x0000000000000000000000000000000000000000',
-    entrypointAddress: '0x0000000000000000000000000000000000000000'
+
+    primaryAccountFactory: '0x7cC54D54bBFc65d1f0af7ACee5e4042654AF8185',
+    entrypointAddress: '0x7079253c0358eF9Fd87E16488299Ef6e06F403B6',
+
+    primaryCardManager: '0xA63DFccB8a39a3DFE4479b33190b12019Ee594E7',
+
+    sessionModuleAddress: '0x51Ef5Add405CCF63c206A80AF8c2B3cEE0282830',
+    sessionFactoryAddress: '0xbE2Cb3358aa14621134e923B68b8429315368E32',
+    sessionProviderAddress: '0x7E72d41A95659257cdED8f8F361742f72396b0fB'
   }
 ];
 
@@ -91,4 +127,36 @@ export const getEntrypointAddressOfChain = (chainId: string) => {
     throw new Error(`Chain with id ${chainId} not found`);
   }
   return chain.entrypointAddress;
+};
+
+export const getPrimaryCardManagerOfChain = (chainId: string) => {
+  const chain = chains.find((chain) => chain.id === chainId);
+  if (!chain) {
+    throw new Error(`Chain with id ${chainId} not found`);
+  }
+  return chain.primaryCardManager;
+};
+
+export const getSessionModuleAddressOfChain = (chainId: string) => {
+  const chain = chains.find((chain) => chain.id === chainId);
+  if (!chain) {
+    throw new Error(`Chain with id ${chainId} not found`);
+  }
+  return chain.sessionModuleAddress;
+};
+
+export const getSessionFactoryAddressOfChain = (chainId: string) => {
+  const chain = chains.find((chain) => chain.id === chainId);
+  if (!chain) {
+    throw new Error(`Chain with id ${chainId} not found`);
+  }
+  return chain.sessionFactoryAddress;
+};
+
+export const getSessionProviderAddressOfChain = (chainId: string) => {
+  const chain = chains.find((chain) => chain.id === chainId);
+  if (!chain) {
+    throw new Error(`Chain with id ${chainId} not found`);
+  }
+  return chain.sessionProviderAddress;
 };
