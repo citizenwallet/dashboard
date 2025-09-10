6ad0a996dd4124ae6f59e988f7c897081c5e8ac9
kev-techi <kevin@techi.be>
2025-09-10 18:18:47 +0200

    get community from db

--- services/cw/index.ts
+++ services/cw/index.ts
@@ -1,11 +1,17 @@
-import communities from './communities.json' assert { type: 'json' };
+import { getServiceRoleClient } from '../top-db';
+import { getCommunityByAlias } from '../top-db/community';
 import { Config } from '@citizenwallet/sdk';
 export const getCommunity = async (
   alias: string
 ): Promise<{ community: Config }> => {
-  const community: Config = communities.find(
-    (community) => community.community.alias === alias
-  ) as unknown as Config;
+  const client = getServiceRoleClient();
+  const { data, error } = await getCommunityByAlias(client, alias);
+
+  if (error || !data) {
+    throw new Error(`Community ${alias} not found`);
+  }
+
+  const community: Config = data.json;
 
   if (!community) throw new Error(`Community ${alias} not found`);
 
