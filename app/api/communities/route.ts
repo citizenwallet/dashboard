01c8020c33a1491818e343802e14c1449908bc38
kev-techi <kevin@techi.be>
2025-09-11 08:39:53 +0200

    support no chain id

--- app/api/communities/route.ts
+++ app/api/communities/route.ts
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
 
   const { data, error } = await getCommunitiesByChainId(
     client,
-    Number(chainId)
+    chainId ? Number(chainId) : undefined
   );
 
   if (error) {
