08c7c74676cb4d4d76e76435d676f6e65dcd5f40
kev-techi <kevin@techi.be>
2025-09-26 11:52:20 +0200

    fix wrong return value for update profile action

--- app/[alias]/(dashboard)/members/[account]/add/profile.tsx
+++ app/[alias]/(dashboard)/members/[account]/add/profile.tsx
@@ -124,7 +124,7 @@ export default function Profile({
           imageFile,
           community.community.alias
         );
-        cid = response.IpfsHash;
+        cid = response || '';
       }
 
       const profile: Profile = {
